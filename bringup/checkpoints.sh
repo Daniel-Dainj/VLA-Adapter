ae69c0bf56f8557f44c906990c2abc008965aaab
Daniel-Dainj <1784910993@qq.com>
2026-03-22 20:34:16 +0800

    更新启动配置

--- bringup/checkpoints.sh
+++ bringup/checkpoints.sh
@@ -1,28 +1,28 @@
 hf download Stanford-ILIAD/prism-qwen25-extra-dinosiglip-224px-0_5b \
 --repo-type model \
---local-dir /media/dainanjun/T9/checkpoints/prism-qwen25-extra-dinosiglip-224px-0_5b/
+--local-dir $HOME/vla_workspace/checkpoints/prism-qwen25-extra-dinosiglip-224px-0_5b/
 
 # rm -rf pretrained_models/prism*
-# ln -sfn /media/dainanjun/T9/checkpoints/prism-qwen25-extra-dinosiglip-224px-0_5b/ pretrained_models/
+# ln -sfn $HOME/vla_workspace/checkpoints/prism-qwen25-extra-dinosiglip-224px-0_5b/ pretrained_models/
 
 hf download VLA-Adapter/LIBERO-Spatial-Pro \
 --repo-type model \
---local-dir /media/dainanjun/T9/checkpoints/VLA-Adapter/LIBERO-Spatial-Pro
+--local-dir $HOME/vla_workspace/checkpoints/VLA-Adapter/LIBERO-Spatial-Pro
 
 hf download VLA-Adapter/LIBERO-Object-Pro \
 --repo-type model \
---local-dir /media/dainanjun/T9/checkpoints/VLA-Adapter/LIBERO-Object-Pro
+--local-dir $HOME/vla_workspace/checkpoints/VLA-Adapter/LIBERO-Object-Pro
 
 hf download VLA-Adapter/LIBERO-Goal-Pro \
 --repo-type model \
---local-dir /media/dainanjun/T9/checkpoints/VLA-Adapter/LIBERO-Goal-Pro
+--local-dir $HOME/vla_workspace/checkpoints/VLA-Adapter/LIBERO-Goal-Pro
 
 hf download VLA-Adapter/LIBERO-Long-Pro \
 --repo-type model \
---local-dir /media/dainanjun/T9/checkpoints/VLA-Adapter/LIBERO-Long-Pro
+--local-dir $HOME/vla_workspace/checkpoints/VLA-Adapter/LIBERO-Long-Pro
 
 hf download VLA-Adapter/CALVIN-ABC-Pro \
 --repo-type model \
---local-dir /media/dainanjun/T9/checkpoints/VLA-Adapter/CALVIN-ABC-Pro
+--local-dir $HOME/vla_workspace/checkpoints/VLA-Adapter/CALVIN-ABC-Pro
 
-# ln -sfn /media/dainanjun/T9/checkpoints/VLA-Adapter checkpoints
+# ln -sfn $HOME/vla_workspace/checkpoints/VLA-Adapter checkpoints
--- bringup/datasets.sh
+++ bringup/datasets.sh
@@ -1,14 +1,17 @@
 hf download openvla/modified_libero_rlds \
   --repo-type dataset \
-  --local-dir /home/dainanjun/data/datasets/libero_rlds \
-  # --include "libero_spatial_no_noops/*"
+  --local-dir $HOME/vla_workspace/datasets/libero_rlds
 
-# ln -sfn /home/dainanjun/data/datasets/libero_rlds third_party/libero/libero/datasets
+# ln -sfn $HOME/vla_workspace/datasets/libero_rlds third_party/libero/libero/datasets
 
 hf download zhouhongyi/calvin_abc_rlds \
   --repo-type dataset \
-  --local-dir /home/dainanjun/data/datasets/calvin_abc_rlds
+  --local-dir $HOME/vla_workspace/datasets/calvin_abc_rlds \
+  --exclude="*0010"{0,1}"-of-00512"
+
+wget -c -P $HOME/vla_workspace/datasets/calvin_abc_rlds https://hf-mirror.com/datasets/zhouhongyi/calvin_abc_rlds/resolve/main/calvin_abc-train.tfrecord-00100-of-00512
+wget -c -P $HOME/vla_workspace/datasets/calvin_abc_rlds https://hf-mirror.com/datasets/zhouhongyi/calvin_abc_rlds/resolve/main/calvin_abc-train.tfrecord-00101-of-00512
 
 # mkdir -p datasets
-# ln -sfn /home/dainanjun/data/datasets/libero_rlds datasets/libero_rlds
-# ln -sfn /media/dainanjun/1A921EB6921E95FB/datasets/calvin_abc_rlds datasets/calvin_abc_rlds
+# ln -sfn $HOME/vla_workspace/datasets/libero_rlds datasets/libero_rlds
+# ln -sfn $HOME/vla_workspace/datasets/calvin_abc_rlds datasets/calvin_abc_rlds
